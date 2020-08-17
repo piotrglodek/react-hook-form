b98ae384c752c362f2c495800642e036a9b45b84
Piotr Glodek <glodekpiotr@wp.pl>
2020-08-17 13:34:37 +0200

    created basic form

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,7 +8,31 @@ const Form = () => {
 
   return (
     <form className='form' onSubmit={handleSubmit(handleFormValidation)}>
-      <Input name='firstName' type='text' label='First name' />
+      <Input
+        name='firstName'
+        type='text'
+        label='First name'
+        register={register}
+      />
+      <Input
+        name='lastName'
+        type='text'
+        label='Last name'
+        register={register}
+      />
+      <Input name='email' type='email' label='Email' register={register} />
+      <Input
+        name='password'
+        type='password'
+        label='Password'
+        register={register}
+      />
+      <Input
+        name='repeatPassword'
+        type='password'
+        label='Repeat password'
+        register={register}
+      />
       <div className='form__control'>
         <input type='submit' className='form__submit' value='Validate form' />
       </div>

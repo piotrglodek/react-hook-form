fe6802e205c3a2cc1592282209f75ebb3e15a0df
Piotr Glodek <glodekpiotr@wp.pl>
2020-08-17 13:12:26 +0200

    Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Form = () => {
-  return <form>form</form>;
+  const { register, handleSubmit, watch, errors } = useForm();
+  const handleFormValidation = (data) => console.log(data);
+
+  return (
+    <form className='form' onSubmit={handleSubmit(handleFormValidation)}>
+      form
+    </form>
+  );
 };
 
 export default Form;
